a1dd8ca7e7a8001b9b94375958452fcf305a23c0
AleRmzMtz <alermzmtz1014@gmail.com>
2016-01-15 11:48:21 -0600

    00-Saludo

---
+++ 00-Saludo/src/Saludo.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+/**
+ * 
+ * @author AleAle Mtz.
+ *
+ */
+public class Saludo {
+/**
+ * 
+ * @param args son los datos que recibe el metodo con los cuales se trabajara
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Hola Ale");
+		System.out.println("Hola desde la consola");
+		
+	}
+
+}
