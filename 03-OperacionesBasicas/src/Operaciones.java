1b7cae5c23d1a9337d8ab94d86630c3426faede1
AleRmzMtz <alermzmtz1014@gmail.com>
2016-01-15 13:52:16 -0600

    03-OperacionesBasicas

---
+++ 03-OperacionesBasicas/src/Operaciones.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author AleAle Mtz
+ *
+ */
+public class Operaciones {
+	/**
+	 * 
+	 * @param n1 en formato entero
+	 * @param n2 en formato entero
+	 * @return regresa el resultado de la suma de dos numeros
+	 */
+	public int sumar(int n1, int n2){
+		return n1+n2;
+	}
+
+}
