2904c68d04be734d587b673166d6e8fdb3dbe9f8
AleRmzMtz <alermzmtz1014@gmail.com>
2016-01-22 08:52:43 -0600

    05-Perimetros

---
+++ 05-Perimetros/src/Perimetros.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author AleAle Mtz
+ *
+ */
+
+/**
+ * 
+ * creacion de una clase que realiza el perimetro de un cuadrado 
+ *
+ */
+public class Perimetros {
+	public int cuadrado(int lado){
+		return lado +lado+lado+lado;
+	}
+
+}
