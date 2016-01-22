b7ef6ffb3eb2096cfe8cc937c002b2d88008cba0
AleRmzMtz <alermzmtz1014@gmail.com>
2016-01-22 08:44:38 -0600

    05-Perimetros

---
+++ 05-Perimetros/src/AppPerimetros.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author AleAle Mtz
+ *
+ */
+public class AppPerimetros {
+	public static void main(String[] args) {
+		Perimetros p= new Perimetros();
+		System.out.printf("El perimetro del cuadrado es: %d ",p.cuadrado(10));
+		
+		
+	}
+}
